f10dbcc97e3030e2a5e9487dce1e487b41f81397
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-21 09:32:50 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,6 +9,6 @@ def run_guessing_game
   elsif input.to_i ==random_num
     puts "You guessed the correct number!"
   else
-    puts "Sorry"
+    puts "Sorry! The computer guessed #{random_num}"
   end
 end
